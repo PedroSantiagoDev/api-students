1cb659742c6b7c8b19f6faab9951e1f2004b7353
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2024-07-24 11:42:11 -0300

    feat: create DB instance in API

--- api/api.go
+++ api/api.go
@@ -6,12 +6,11 @@ import (
 	"github.com/PedroSantiagoDev/api-students/db"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"gorm.io/gorm"
 )
 
 type API struct {
 	Echo *echo.Echo
-	DB   *gorm.DB
+	DB   *db.StudentHandler
 }
 
 func NewServer() *API {
@@ -20,41 +19,42 @@ func NewServer() *API {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	db := db.Init()
+	database := db.Init()
+	studentDB := db.NewStudentHandler(database)
 
 	return &API{
 		Echo: e,
-		DB:   db,
+		DB:   studentDB,
 	}
 }
 
 func (api *API) ConfigureRoutes() {
-	api.Echo.GET("/students", getStudents)
-	api.Echo.POST("/students", createStudents)
-	api.Echo.GET("/students/:id", getStudents)
-	// api.Echo.PUT("/students/:id", updateStudents)
-	// api.Echo.DELETE("/students/:id", deleteStudents)
+	api.Echo.GET("/students", api.getStudents)
+	api.Echo.POST("/students", api.createStudents)
+	//api.Echo.GET("/students/:id", api.getStudent)
+	// api.Echo.PUT("/students/:id", api.updateStudents)
+	// api.Echo.DELETE("/students/:id", api.deleteStudents)
 }
 
 func (api *API) Start() error {
 	return api.Echo.Start(":8080")
 }
 
-func getStudents(c echo.Context) error {
-	students, err := db.GetStudent()
+func (api *API) getStudents(c echo.Context) error {
+	students, err := api.DB.GetStudent()
 	if err != nil {
 		return c.String(http.StatusNotFound, "Failed to get student")
 	}
 	return c.JSON(http.StatusOK, students)
 }
 
-func createStudents(c echo.Context) error {
+func (api *API) createStudents(c echo.Context) error {
 	student := db.Student{}
 	if err := c.Bind(&student); err != nil {
 		return err
 	}
 
-	if err := db.AddStudent(student); err != nil {
+	if err := api.DB.AddStudent(student); err != nil {
 		return c.String(http.StatusInternalServerError, "Error to create students")
 	}
 	return c.String(http.StatusCreated, "Create students")
--- db/db.go
+++ db/db.go
@@ -8,6 +8,10 @@ import (
 	"gorm.io/gorm"
 )
 
+type StudentHandler struct {
+	DB *gorm.DB
+}
+
 type Student struct {
 	gorm.Model
 	Name   string `json:"name"`
@@ -27,20 +31,20 @@ func Init() *gorm.DB {
 	return db
 }
 
-func AddStudent(student Student) error {
-	db := Init()
+func NewStudentHandler(db *gorm.DB) *StudentHandler {
+	return &StudentHandler{DB: db}
+}
 
-	if res := db.Create(&student); res.Error != nil {
+func (s *StudentHandler) AddStudent(student Student) error {
+	if res := s.DB.Create(&student); res.Error != nil {
 		return res.Error
 	}
-
 	fmt.Println("Create student")
 	return nil
 }
 
-func GetStudent() ([]Student, error) {
+func (s *StudentHandler) GetStudent() ([]Student, error) {
 	students := []Student{}
-	db := Init()
-	err := db.Find(&students).Error
+	err := s.DB.Find(&students).Error
 	return students, err
 }
