f54f82c465ff674c703ecdbe877309703f5f34f3
Pedro Santiago <joaopedrosantiago1103@gmail.com>
2024-07-24 13:26:57 -0300

    Improve logs

--- main.go
+++ main.go
@@ -1,15 +1,14 @@
 package main
 
 import (
-	"log"
-
 	"github.com/PedroSantiagoDev/api-students/api"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
 	server := api.NewServer()
 	server.ConfigureRoutes()
 	if err := server.Start(); err != nil {
-		log.Fatal(err)
+		log.Fatal().Err(err).Msg("Failed to start server")
 	}
 }
